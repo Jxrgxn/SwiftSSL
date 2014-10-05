057c66b78625db5701c57095f519b0b83bce50e6
0day2010 <0day.zh@gmail.com>
2014-10-06 07:27:22 +0800

    Added project.

---
+++ SwiftSSL/SwiftSSL.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSSL.h
+//  SwiftSSL
+//
+//  Created by 0day on 14/10/6.
+//  Copyright (c) 2014年 SwiftP2P. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSSL.
+FOUNDATION_EXPORT double SwiftSSLVersionNumber;
+
+//! Project version string for SwiftSSL.
+FOUNDATION_EXPORT const unsigned char SwiftSSLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSSL/PublicHeader.h>
+
+
