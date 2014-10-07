4fa690b36c278add6ab715faa6cdae963198b81c
0day2010 <0day.zh@gmail.com>
2014-10-07 17:02:25 +0800

    Added Playground && Added SHA256 method.

--- SwiftSSL/HMAC.swift
+++ SwiftSSL/HMAC.swift
@@ -55,6 +55,62 @@ enum HMACAlgorithm {
     }
 }
 
+/**
+ * SHA256
+ */
 extension NSData {
+    public func SHA256Digest() -> NSData {
+        let digestLength = HMACAlgorithm.SHA256.digestLength()
+        var hash = [UInt8](count: digestLength, repeatedValue: 0)
+        
+        CC_SHA256(self.bytes, UInt32(self.length), &hash)
+        
+        return NSData(bytes: hash, length: digestLength)
+    }
     
+    public func SHA256HexStringDigest() -> String {
+        
+    }
+    
+    public func hexStringValue() -> String {
+        let sha256description = self.description as String
+        
+        // TODO: more elegant way to convert NSData to a hex string
+        
+        var result: String = ""
+        
+        for char in sha256description {
+            switch char {
+            case "0", "1", "2", "3", "4", "5", "6", "7","8","9", "a", "b", "c", "d", "e", "f":
+                result.append(char)
+            default:
+                result += ""
+            }
+        }
+        
+        return result
+    }
+}
+
+extension String {
+    public func dataValueFromThisHexString() -> NSData {
+        // Based on: http://stackoverflow.com/a/2505561/313633
+        var data = NSMutableData()
+        
+        var string = ""
+        
+        for char in self {
+            string.append(char)
+            if(countElements(string) == 2) {
+                let scanner = NSScanner(string: string)
+                var value: CUnsignedInt = 0
+                scanner.scanHexInt(&value)
+                data.appendBytes(&value, length: 1)
+                string = ""
+            }
+            
+        }
+        
+        return data as NSData
+    }
 }
